31bb9437f04e1779e42de95cfa1306a88b7815aa
Samir Soormally <samirsoormally@gmail.com>
2018-09-18 15:00:40 +0100

    refactored tests for notes-list-spec.js

--- spec/notes-list-spec.js
+++ spec/notes-list-spec.js
@@ -1,7 +1,23 @@
 describe("Notes list", function() {
+
+beforeEach(function() {
   nl = notesList();
   nl.addNote("Hello1");
   nl.addNote("Hello2");
   nl.addNote("Hello3");
-  it("stores a list of notes", function() {});
+});
+
+  describe(".showNote", function() {
+    it("shows all notes", function() {
+      expect(nl.showNotes()[0].text()).toEqual("Hello1")
+      expect(nl.showNotes()[1].text()).toEqual("Hello2")
+      expect(nl.showNotes()[2].text()).toEqual("Hello3")
+    });
+  });
+  describe(".addNote", function() {
+    it("adds a note", function() {
+      nl.addNote("Hello4");
+      expect(nl.showNotes()[3].text()).toEqual("Hello4")
+    });
+  });
 });
